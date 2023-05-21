1549424ec3ae03f87b02337889fce33676da7ba0
DanielMorales1103 <mor21785@uvg.edu.gt>
2023-05-20 18:20:05 -0600

    Nombres agregados

--- pokemonBattle/src/scenes/game.js
+++ pokemonBattle/src/scenes/game.js
@@ -29,6 +29,11 @@ export default async function(pokemon, opponent) {
         scale(3.5), 
         'player'
     ]);
+    add([
+        text(pokemon.name, 32),
+        pos(width() / 2 + 450, height()/2+7),
+        color(0, 0, 0), // Cambia el color del texto a negro (RGB: 0, 0, 0)
+    ]);
 
     let secondPlayer = add([
         sprite(opponent.sprite),
@@ -36,5 +41,11 @@ export default async function(pokemon, opponent) {
         scale(2.2), 
         'opponent'
     ]);
+    add([
+        text(opponent.name, 32),
+        pos(width()/15, 40),
+        color(0, 0, 0), // Cambia el color del texto a negro (RGB: 0, 0, 0)
+    ]);
+    
 
 }
