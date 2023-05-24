bbad3521c4e2964b11fdfb6617cc1cb8a1db4615
DanielMorales1103 <mor21785@uvg.edu.gt>
2023-05-24 16:30:46 -0600

    Multiplicador a función async

--- pokemonBattle/src/utilities/utilities.js
+++ pokemonBattle/src/utilities/utilities.js
@@ -1,43 +1,48 @@
-export function calculadorMultiplicador(pokemon1, pokemon2){
-    //fortalezas y debilidades por tipo
-    let tipoRelaciones = {
-        normal: { weaknesses: [], strengths: [] },
-        fire: { weaknesses: ["water", "rock", "ground"], strengths: ["grass", "ice", "bug", "steel"] },
-        water: { weaknesses: ["electric", "grass"], strengths: ["fire", "rock", "ground"] },
-        electric: { weaknesses: ["ground"], strengths: ["water", "flying"] },
-        grass: { weaknesses: ["fire", "ice", "poison", "flying", "bug"], strengths: ["water", "ground", "rock"] },
-        ice: { weaknesses: ["fire", "fighting", "rock", "steel"], strengths: ["grass", "ground", "flying", "dragon"] },
-        fighting: { weaknesses: ["flying", "poison", "psychic", "bug", "fairy"], strengths: ["normal", "ice", "rock", "steel"] },
-        poison: { weaknesses: ["ground", "psychic"], strengths: ["grass", "fairy"] },
-        ground: { weaknesses: ["water", "grass", "ice"], strengths: ["electric", "poison", "fire", "rock", "steel"] },
-        flying: { weaknesses: ["electric", "ice", "rock"], strengths: ["grass", "fighting", "bug"] },
-        psychic: { weaknesses: ["bug", "ghost", "dark"], strengths: ["fighting", "poison"] },
-        bug: { weaknesses: ["fire", "flying", "rock"], strengths: ["grass", "psychic", "poison"] },
-        rock: { weaknesses: ["water", "grass", "fighting", "ground", "steel"], strengths: ["fire", "ice", "bug", "flying", "fairy"] },
-        ghost: { weaknesses: ["ghost", "dark"], strengths: ["psychic"] },
-        dragon: { weaknesses: ["fairy", "dragon"], strengths: ["dragon"] }
-      }
-
-    
-
-    let multiplicador = 1;
-    for (let i = 0; i < pokemon1.length; i++){
-        let tipo = pokemon1[i]
-        let relaciones = tipoRelaciones[tipo]
-        //console.log(relaciones)
-        let fortalezas = relaciones.strengths
-        let debilidades = relaciones.weaknesses
+export async function calculadorMultiplicador(pokemon1, pokemon2){
+    try{
+        //fortalezas y debilidades por tipo
+        let tipoRelaciones = {
+            normal: { weaknesses: [], strengths: [] },
+            fire: { weaknesses: ["water", "rock", "ground"], strengths: ["grass", "ice", "bug", "steel"] },
+            water: { weaknesses: ["electric", "grass"], strengths: ["fire", "rock", "ground"] },
+            electric: { weaknesses: ["ground"], strengths: ["water", "flying"] },
+            grass: { weaknesses: ["fire", "ice", "poison", "flying", "bug"], strengths: ["water", "ground", "rock"] },
+            ice: { weaknesses: ["fire", "fighting", "rock", "steel"], strengths: ["grass", "ground", "flying", "dragon"] },
+            fighting: { weaknesses: ["flying", "poison", "psychic", "bug", "fairy"], strengths: ["normal", "ice", "rock", "steel"] },
+            poison: { weaknesses: ["ground", "psychic"], strengths: ["grass", "fairy"] },
+            ground: { weaknesses: ["water", "grass", "ice"], strengths: ["electric", "poison", "fire", "rock", "steel"] },
+            flying: { weaknesses: ["electric", "ice", "rock"], strengths: ["grass", "fighting", "bug"] },
+            psychic: { weaknesses: ["bug", "ghost", "dark"], strengths: ["fighting", "poison"] },
+            bug: { weaknesses: ["fire", "flying", "rock"], strengths: ["grass", "psychic", "poison"] },
+            rock: { weaknesses: ["water", "grass", "fighting", "ground", "steel"], strengths: ["fire", "ice", "bug", "flying", "fairy"] },
+            ghost: { weaknesses: ["ghost", "dark"], strengths: ["psychic"] },
+            dragon: { weaknesses: ["fairy", "dragon"], strengths: ["dragon"] }
+        }
         
-        //revisar tipos del segundo pokemon
-        for(let j= 0; j< pokemon2.length;j++){
-            let tipo2 = pokemon2[j]
-            if(fortalezas.includes(tipo2)){
-                multiplicador = multiplicador*2;
-            }
-            if(debilidades.includes(tipo2)){
-                multiplicador = multiplicador*0.5
+
+        let multiplicador = 1;
+        for (let i = 0; i < pokemon1.length; i++){
+            let tipo = pokemon1[i]
+            let relaciones = tipoRelaciones[tipo]
+            //console.log(relaciones)
+            let fortalezas = relaciones.strengths
+            let debilidades = relaciones.weaknesses
+            
+            //revisar tipos del segundo pokemon
+            for(let j= 0; j< pokemon2.length;j++){
+                let tipo2 = pokemon2[j]
+                if(fortalezas.includes(tipo2)){
+                    multiplicador = multiplicador*2;
+                }
+                if(debilidades.includes(tipo2)){
+                    multiplicador = multiplicador*0.5
+                }
             }
         }
+        return multiplicador
+
+    } catch (error) {
+        throw new Error('Error fetching Pokemon list:', error);
     }
-    return multiplicador
+    
 }
